9cb1799b531308f8eb1ba5c7dd729d04312e970d
Ester Costa de Souza <alwesterc@gmail.com>
2023-06-20 20:24:02 -0300

    Text Component: fontSize and lineHeight

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  parser: 'flow',
+  bracketSpacing: true,
 };
