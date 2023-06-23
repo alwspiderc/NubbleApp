3681e535941403d474166e53d71dbb6902853e5f
Ester Costa de Souza <alwesterc@gmail.com>
2023-06-23 10:28:11 -0300

    icon lib with react-native-svg

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { Text } from './src/components/Text/Text';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
 import { Button } from './src/components/Button/Button';
+import { Icon } from './src/components/Icon/Icon';
+import { Box } from './src/components/Box/Box';
 
 function App(): JSX.Element {
   return (
@@ -13,11 +15,27 @@ function App(): JSX.Element {
           <Text preset="headingLarge" italic>
             Coffstack
           </Text>
+          <Box flexDirection="row">
+            <Icon name="chevronRight" color="successLight" size={50} />
+            <Icon name="heartFill" color="buttonPrimary" />
+            <Icon name="profile" size={50} />
+            <Icon name="profileFill" size={50} />
+            <Icon name="heart" size={50} />
+            <Icon name="bellOn" color="carrotSecondary" size={50} />
+          </Box>
+          <Box flexDirection="row">
+            <Icon name="newPost" size={50} />
+            <Icon name="camera" size={50} />
+            <Icon name="chat" size={50} />
+            <Icon name="chatOn" color="error" size={50} />
+            <Icon name="flashOff" size={50} />
+            <Icon name="flashOn" size={50} />
+          </Box>
 
-          <Button title="primary" marginBottom="s12" marginTop="s16" />
-          <Button disabled title="primary" marginBottom="s12" marginTop="s16" />
+          {/* <Button title="primary" marginBottom="s12" marginTop="s16" />
+          <Button disabled title="primary" marginBottom="s12" marginTop="s16" /> */}
 
-          <Button
+          {/* <Button
             preset="outline"
             title="outline"
             marginBottom="s12"
@@ -39,7 +57,7 @@ function App(): JSX.Element {
             marginBottom="s12"
             marginTop="s16"
             loading
-          />
+          /> */}
         </View>
       </SafeAreaView>
     </ThemeProvider>
