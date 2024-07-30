abd643f6c26a8a12c42e3f8c0e3e235e9f73b5e6
Ester Costa de Souza <alwesterc@gmail.com>
2024-07-30 15:43:55 -0300

    feat: post item draft

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
 
-import {Button, Screen, Text} from '@components';
+import {Box, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 import {Post, postService} from '@domain';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
-export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
+export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
   const [postList, setPostList] = useState<Post[]>([]);
 
   useEffect(() => {
@@ -13,11 +14,32 @@ export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
     });
   }, []);
 
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box mb="s24">
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            style={{width: 32, height: 32}}
+          />
+          <Text>{item.author.name}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          resizeMode="cover"
+          style={{width: Dimensions.get('screen').width, height: 300}}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      {postList.map(post => (
-        <Text key={post.id}>{post.text}</Text>
-      ))}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 }
