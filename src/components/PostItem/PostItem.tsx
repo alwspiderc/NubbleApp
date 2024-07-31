e49c8615514048d4f0791f07ed4160a6397f5654
Ester Costa de Souza <alwesterc@gmail.com>
2024-07-31 14:36:58 -0300

    feat: PostBottom

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -4,6 +4,7 @@ import {Post} from '@domain';
 import {PostHeader} from './components/PostHeader';
 import {PostImage} from './components/PostImage';
 import {PostActions} from './components/PostActions';
+import {PostBottom} from './components/PostBottom';
 
 interface Props {
   post: Post;
@@ -17,6 +18,11 @@ export function PostItem({post}: Props) {
       <PostActions
         reactionCount={post.reactionCount}
         favoriteCount={post.favoriteCount}
+        commentCount={post.commentCount}
+      />
+      <PostBottom
+        author={post.author}
+        text={post.text}
         commentCount={post.commentCount}
       />
     </Box>
