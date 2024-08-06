12060b1251758847d6044ff9a97091e5d30c9810
Ester Costa de Souza <alwesterc@gmail.com>
2024-08-06 18:46:52 -0300

    arquitetura do post adapter

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -1,9 +1,10 @@
 import {postApi} from './postApi';
 import {Post} from './postTypes';
+import {postAdapter} from './postAdapter';
 
 async function getList(): Promise<Post[]> {
-  const postList = await postApi.getList();
-  return postList;
+  const postPageAPI = await postApi.getList();
+  return postPageAPI.data.map(postAdapter.toPost);
 }
 
 export const postService = {
